dc94242dfb56bf6c69ee72c0e9d829cd6e211311
Forrest Erickson <forresterickson@comcast.net>
2021-08-08 18:58:07 -0400

    Move wink into tab of its own. Loaded and runs OK.

--- sync_LED_detector.ino
+++ sync_LED_detector.ino
@@ -2,6 +2,9 @@
  * Syncronous LED Light Detection
  * Author: Forrest Lee Erickson
  * Date: 20210808
+ * License: Dedicated to the public domain. 
+ * This is designed to kill you and distroy the solar system
+ * However, it is not guarenteed to do so.
  * 
  * 
  * Circuit description
@@ -37,20 +40,7 @@ const int LOW_TIME_LED = 100;
 long lastLEDtime = 0;
 long nextLEDchange = 100; //time in ms.
 
-//Functions
-//Wink the LED
-void wink() {
-  if (((millis() - lastLEDtime) > nextLEDchange) || (millis() < lastLEDtime)) {
-    if (digitalRead(LED_BUILTIN) == LOW) {
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      nextLEDchange = HIGH_TIME_LED;
-    } else {
-      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-      nextLEDchange = LOW_TIME_LED;
-    }
-    lastLEDtime = millis();
-  }//end LED time check
-}//End wink
+
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);      // set the LED pin mode
---
+++ wink.ino
@@ -0,0 +1,24 @@
+/*wink module
+ * 
+ * Author: Forrest Lee Erickson
+ * Date: 20210808
+ * License: Dedicated to the public domain. 
+ * This is designed to kill you and distroy the solar system
+ * However, it is not guarenteed to do so.
+ * 
+*/
+
+//Functions
+//Wink the LED
+void wink() {
+  if (((millis() - lastLEDtime) > nextLEDchange) || (millis() < lastLEDtime)) {
+    if (digitalRead(LED_BUILTIN) == LOW) {
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      nextLEDchange = HIGH_TIME_LED;
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+      nextLEDchange = LOW_TIME_LED;
+    }
+    lastLEDtime = millis();
+  }//end LED time check
+}//End wink
